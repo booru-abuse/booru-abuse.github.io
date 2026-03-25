6b69ca0302987874a5fe564060823b2230798a1c
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-03-24 20:45:33 -0500

    feat: expect error and modify eval

--- src/rule34/main.ts
+++ src/rule34/main.ts
@@ -1,9 +1,10 @@
+//@ts-expect-error TS2307
 import * as Boorujs from "https://esm.sh/gh/boorujs/booru.js@8c561f1/src/index.ts.mjs?target=es2022";
 import { Submodule } from "../all/template-class.ts";
 
 const { Rule34 } = Boorujs;
 
-window.eval(`\`
+window.eval(`/*
 
 this page is in testing and i just want to be sure esm.sh works before i get
 custom api keys or a vercel service set up
@@ -13,7 +14,7 @@ refresh the key for the account once i get custom keys set up #lol
 
 regardless dont abuse the given key please and thanks!
 
-\``);
+*/`);
 
 const auth = {
     api_key: "57931764243134609eb715b9d5c931134cb3cadfdbe166548b2b25543b869f82ee435076819810388fac48119b1eb0731dc86d75aff7b163e5d0c01f21618ecb",
